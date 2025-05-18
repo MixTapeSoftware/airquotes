10e4ed6f455836347c1f13823c18f406e46db78d
Chad Fennell <chadfennell@users.noreply.github.com>
2025-05-18 08:53:46 -0700

    removes some debugging code

--- app/temporal/activities/parse_quote.rb
+++ app/temporal/activities/parse_quote.rb
@@ -3,29 +3,10 @@ class ParseQuote < Temporalio::Activity::Definition
     puts "=== Starting ParseQuote activity ==="
     puts "Quote ID: #{quote_id}"
 
-    begin
-      puts "Finding quote..."
-      quote = Quote.find(quote_id)
-      puts "Found quote: #{quote.inspect}"
-
-      puts "Getting filepath..."
-      filepath = FileUploaderService.path_for(quote.filename)
-      puts "Filepath: #{filepath}"
-
-      puts "Calling LlamaCloudService..."
-      result = LlamaCloudService.process_document(filepath)
-      puts "LlamaCloud result: #{result.inspect}"
-
-      if result.nil?
-        puts "WARNING: LlamaCloud returned nil result"
-      end
-
-      puts "=== Activity complete ==="
-      result
-    rescue => e
-      puts "ERROR in ParseQuote activity: #{e.message}"
-      puts e.backtrace.join("\n")
-      raise
-    end
+    quote = Quote.find(quote_id)
+    filepath = FileUploaderService.path_for(quote.filename)
+    result = LlamaCloudService.process_document(filepath)
+    quote.update(parsed_result: result)
+    quote_id
   end
 end
